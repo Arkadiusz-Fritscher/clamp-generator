c4c478aa4b843e1eda8fbfcab6ff09e279080e89
Arek <arkadiuszp@arcor.de>
2022-03-05 11:34:53 +0100

    add option to change base font size and reset the store

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,44 @@
 <template>
-  <header class="flex h-20 items-center px-base-padding-inline">
-    This is the Header
+  <header class="flex h-20 items-center gap-x-6 px-base-padding-inline">
+    <div class="h-8 w-auto">
+      <IconLogo />
+    </div>
+    <div class="ml-auto flex items-center gap-x-1">
+      <span class="text-caption text-clamp-dark-blue-200">Basefontsize</span>
+      <BaseInput
+        class="max-w-[4ch]"
+        id="baseFont"
+        :base-input="store.baseFont.value"
+        @change="changeBaseFont"
+      />
+      <span class="text-caption text-clamp-dark-blue-200">px</span>
+    </div>
+    <div class="flex items-center gap-x-1">
+      <span class="text-caption text-clamp-dark-blue-200">Reset</span>
+      <IconRefresh @click="resetStore" />
+    </div>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import IconLogo from "./icons/IconLogo.vue";
+import IconRefresh from "./icons/IconRefresh.vue";
+import BaseInput from "./base/BaseInput.vue";
+import { useStore } from "../stores/store";
+
+const store = useStore();
+
+const changeBaseFont = (event) => {
+  const input = +event.target.value;
+  if (input > 1) {
+    store.baseFont.value = input;
+  }
+};
+
+const resetStore = () => {
+  store.$reset();
+};
+</script>
 
 <style scoped>
 header {
--- src/components/card/CurrentClampCard.vue
+++ src/components/card/CurrentClampCard.vue
@@ -1,16 +1,17 @@
 <template>
   <BaseCard title="Current clamp">
     <div
+      v-if="store.currentClamp"
       class="flex items-center justify-center gap-x-4 rounded px-base-padding-inline"
     >
       <div
-        class="clamp-string flex h-14 items-center justify-center rounded bg-clamp-dark-blue-300 py-6 px-12 font-semibold"
+        class="flex h-14 items-center justify-center rounded bg-clamp-dark-blue-300 py-6 px-12 font-semibold"
       >
         {{ store.currentClamp.string }}
       </div>
       <div>
         <button class="btn">
-          <IconCopy />
+          <IconCopy @click="copyClamp" />
         </button>
       </div>
     </div>
@@ -22,6 +23,25 @@ import { useStore } from "../../stores/store";
 import BaseCard from "../base/BaseCard.vue";
 import IconCopy from "../icons/IconCopy.vue";
 const store = useStore();
+
+const copyClamp = (event) => {
+  navigator.clipboard.writeText(store.currentClamp.string).then(
+    () => {
+      store.addMessage({
+        message: "Value copy",
+        type: "success",
+        pos: { x: event.pageX, y: event.pageY },
+      });
+    },
+    () => {
+      store.addMessage({
+        message: "Copy fail",
+        type: "error",
+        pos: { x: event.pageX, y: event.pageY },
+      });
+    }
+  );
+};
 </script>
 
 <style scoped>
@@ -33,10 +53,6 @@ const store = useStore();
 ::selection {
   color: var(--color-dark-blue-400);
   background: var(--color-blue-white-50);
-}
-
-.clamp-string {
-  font-size: clamp(1rem, -0.126rem + 1.835vw, 1.25rem);
 }
 
 .btn {
